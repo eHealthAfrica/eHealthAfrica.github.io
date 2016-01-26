eb4eab289aa97e52b0374e90a433821297888245
adamvert <ab@adam-butler.com>
2016-01-26 10:25:12 +0100

    Integrate feature.js to test for `srcset` and `sizes` attributes

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -1,4 +1,5 @@
 var attachFastClick = require('fastclick')
+var feature = require('./Feature')
 
 var $ = window.$
 
@@ -26,20 +27,38 @@ $(function () {
     }
   })
 
-  // add srcset attribute to img tags, where possible, and include alternative URLs
-  if (window.feature.srcset) {
-    $('img').each(function () {
-      var $img = $(this)
-      var src = $img.attr('src')
+  $('img').each(function () {
+    var $img = $(this)
+    var pathNoExtension = $img.attr('src').replace(/\.[^.]*$/, '')
 
-      // 320, 640, 1280, 2560
+    // check whether this image is available in various sizes
+    checkFileExists(pathNoExtension + '-320.jpg')
+      .fail(function () {
+        console.log('This 404 error is normal; it\'s just a result of testing for responsive images')
+      })
+      .done(function () {
+        // add srcset attribute to img tags, where possible, and include alternative URLs
+        if (feature.srcset) {
+          var widths = [320, 640, 720, 1440]
+          var sizes = '100vw, (min-width: 800px) 720px, 100vw'
+          var srcset = ''
+          for (var i = 0; i < widths.length; i++) {
+            srcset += pathNoExtension + '-' + widths[i] + '.jpg ' + widths[i] + 'w'
+            if (i < widths.length - 1) {
+              srcset += ', '
+            }
+          }
+          $img.attr('srcset', srcset)
 
-
-      if (imageExists(src, '@2x')) {
-
-      }
-    })
-  }
+          if (feature.sizes) {
+            $img.attr('sizes', sizes)
+          }
+        } else {
+          // if no browser support, set a medium sized image
+          $img.attr('src', pathNoExtension + '-720.jpg')
+        }
+      })
+  })
 
   // show/hide nav on mobile
   $('.js-show-nav').click(function () {
@@ -58,6 +77,9 @@ function scrollTo ($el) {
   }, 'fast')
 }
 
-function imageExists (src, variant) {
-  return true;
+function checkFileExists (src) {
+  return $.ajax({
+    url: src,
+    type: 'HEAD'
+  })
 }
